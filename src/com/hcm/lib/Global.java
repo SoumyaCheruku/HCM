5fcfce9c64d35be3d90a1926786590835aac45ad
Soumya Cheruku <soumya.cheruku@gmail.com>
2019-07-20 06:44:39 +0000

    commit hcm

--- src/com/hcm/lib/Global.java
+++ src/com/hcm/lib/Global.java
@@ -3,7 +3,7 @@ package com.hcm.lib;
 import org.openqa.selenium.WebDriver;
 
 public class Global {
-	public WebDriver d1;	
-	public String url="http://192.168.2.140:8282/thcm/logout";
+	public WebDriver driver;	
+	public String url="http://192.168.2.245:8181";
 	
 }
